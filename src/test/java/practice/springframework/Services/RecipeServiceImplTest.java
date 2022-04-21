9c17d8d91a0b39921a3d780097ec902df73b4a8f
huyiqing1 <huyiqing1@gmail.com>
2022-04-21 13:38:55 -0400

    create some unit tests

---
+++ src/test/java/practice/springframework/Services/RecipeServiceImplTest.java
@@ -0,0 +1,41 @@
+package practice.springframework.Services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import practice.springframework.Models.Recipe;
+import practice.springframework.Repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    RecipeServiceImpl recipeService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet<>();
+        recipesData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
