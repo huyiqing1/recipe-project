2e452a209bf2ffdc5e4aef5d2a963351d47c39dc
huyiqing1 <huyiqing1@gmail.com>
2022-05-02 16:02:52 -0400

    added 'add new recipe' function

--- src/main/java/practice/springframework/Controllers/RecipeController.java
+++ src/main/java/practice/springframework/Controllers/RecipeController.java
@@ -3,9 +3,8 @@ package practice.springframework.Controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import practice.springframework.Commands.RecipeCommand;
 import practice.springframework.Services.RecipeService;
 
 @Slf4j
@@ -21,5 +20,19 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
